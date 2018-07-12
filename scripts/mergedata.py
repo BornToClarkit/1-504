4a4068e03c0cb294d5ef418507bc32856e23cdee
Connor Clark <buckeyeb1b@gmail.com>
2018-07-11 23:00:29 -0600

    updated merge to filter out extraneous

--- scripts/mergedata.py
+++ scripts/mergedata.py
@@ -12,8 +12,17 @@ for ticker in tickers:
 	data = list(reader(open("data/"+str(i)+".csv"),delimiter=","))
 	data.pop(0)
 	for row in data:
+		row.append(ticker)
+		change = float(row[4])-float(row[1])
+		row.append(change)
+		if(change > 0):
+			row.append(1)
+		elif(change == 0):
+			row.append(0)
+		elif(change <0):
+			row.append(-1)
 		print("file: " + str(i) +" date: " +row[0])
-		out = writer(open("data/0/"+row[0]+".csv","a"),delimiter=",")
+		out = writer(open("data/1/"+row[0]+".csv","a"),delimiter=",")
 		out.writerow(row)
 	i+=1
 	
